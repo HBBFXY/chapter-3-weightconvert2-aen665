a1ae8cc8acca8ee5248ebf76298953a633f03615
aen665 <2328178360@qq.com>
2025-09-13 17:45:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # 在这个文件里编写代码
+weight=input("请输入你在地球上的体重")
+w=float(weight)
+m=float(weight)
+for i in range(1,11):
+    w+=0.5
+    print("第{}年地球上的体重为{:.2f}".format(i,w))
+for i in range(1,11):
+    m+=0.5
+    moonweight=m*0.165
+    print("第{}年月球上的体重为{:.2f}".format(i,moonweight))
